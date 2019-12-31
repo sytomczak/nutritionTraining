54c6101517d5bacb1efcb41e9ade2d9f5bdffac1
sytomczak <sylwia.tomczak95@gmail.com>
2019-12-31 14:04:43 +0100

    Added button on photoLabel in atlas view

--- src/main/java/pl/sytomczak/nutritiontraining/gui/Atlas.java
+++ src/main/java/pl/sytomczak/nutritiontraining/gui/Atlas.java
@@ -1,6 +1,7 @@
 package pl.sytomczak.nutritiontraining.gui;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -22,5 +23,60 @@ public class Atlas extends JDialog {
                 dispose();
             }
         });
+        photo1jLabel.setLayout(new OverlayLayout(photo1jLabel));
+
+        JButton button = new JButton("Show Message");
+        button.setAlignmentX(-1000);
+        button.setAlignmentY(-1000);
+
+        JPanel popupPanel = createPopupPanel(button);
+        popupPanel.setAlignmentX(100);
+        popupPanel.setAlignmentY(100);
+
+        button.addActionListener(e -> {
+            button.setEnabled(false);
+            popupPanel.setVisible(true);
+        });
+
+        photo1jLabel.add(popupPanel);
+
+        photo1jLabel.add(button);
     }
-};
+
+    private static JPanel createPopupPanel(JComponent overlapComponent) {
+        JPanel popupPanel = new JPanel(new BorderLayout());
+        popupPanel.setOpaque(false);
+        popupPanel.setMaximumSize(new Dimension(300, 300));
+        popupPanel.setBorder(new LineBorder(Color.gray));
+        popupPanel.setVisible(false);
+
+        JLabel label = new JLabel("HI there!");
+        popupPanel.add(wrapInPanel(label), BorderLayout.CENTER);
+
+        JButton popupCloseButton = new JButton("Close");
+        popupPanel.add(wrapInPanel(popupCloseButton), BorderLayout.SOUTH);
+
+        popupCloseButton.addActionListener(e -> {
+            overlapComponent.setEnabled(true);
+            popupPanel.setVisible(false);
+        });
+
+        return popupPanel;
+    }
+
+    private static JPanel wrapInPanel(JComponent component) {
+        JPanel jPanel = new JPanel();
+        jPanel.setBackground(new Color(50, 210, 250, 150));
+        jPanel.add(component);
+        return jPanel;
+    }
+
+
+    private static JFrame createFrame() {
+        JFrame frame = new JFrame("OverlayLayout Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(700, 700));
+        return frame;
+    }
+}
+
