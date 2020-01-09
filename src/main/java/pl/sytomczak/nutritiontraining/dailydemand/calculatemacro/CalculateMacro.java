f6b3565473a888e07ed68b09accb3de76ab4d8bf
sytomczak <sylwia.tomczak95@gmail.com>
2020-01-09 14:09:35 +0100

    Started fixed CalculateMacroTest

--- src/main/java/pl/sytomczak/nutritiontraining/dailydemand/calculatemacro/CalculateMacro.java
+++ src/main/java/pl/sytomczak/nutritiontraining/dailydemand/calculatemacro/CalculateMacro.java
@@ -6,41 +6,106 @@ public class CalculateMacro {
     private int protein;
     private int fat;
     private double carbs;
-
-    private double caloriesDependingOnWeightChange;
+    private VariableCaloriesDependingOnWeightChange caloriesDependingOnWeightChange;
 
     private DailyDemandCalculation dailyDemandCalculation;
     private DailyDemandCalculation weight;
 
-    public double CalculateMacro(VariableCaloriesDependingOnWeightChange variableCaloriesDependingOnWeightChange) {
+    public CalculateMacroResult calculationMacro() {
+        int proteinResult = calculateDailyProtein();
+        double carbsResult = calculateDailyCarbs();
+        int fatResult = calculateDailyFat();
+        double calories = calculateMacro(caloriesDependingOnWeightChange);
+        return new CalculateMacroResult(carbsResult, proteinResult, fatResult, calories);
+    }
+
+    private double calculateMacro(VariableCaloriesDependingOnWeightChange caloriesDependingOnWeightChange) {
         dailyDemandCalculation = new DailyDemandCalculation();
+        double calculateCalories = 0;
 
-        if (VariableCaloriesDependingOnWeightChange.LOSE.equals(variableCaloriesDependingOnWeightChange)) {
-            return caloriesDependingOnWeightChange = dailyDemandCalculation.getDailyDemand() - VariableCaloriesDependingOnWeightChange.LOSE.getNumberOfCaloriesDependingOnWeightChange();
-        } else if (VariableCaloriesDependingOnWeightChange.GAIN.equals(variableCaloriesDependingOnWeightChange)) {
-            return caloriesDependingOnWeightChange = dailyDemandCalculation.getDailyDemand() + VariableCaloriesDependingOnWeightChange.GAIN.getNumberOfCaloriesDependingOnWeightChange();
-        } else return caloriesDependingOnWeightChange = dailyDemandCalculation.getDailyDemand();
+        switch (caloriesDependingOnWeightChange) {
+            case LOSE:
+                 calculateCalories = dailyDemandCalculation.getDailyDemand() - VariableCaloriesDependingOnWeightChange.LOSE.getNumberOfCaloriesDependingOnWeightChange();
+                break;
+            case GAIN:
+                calculateCalories = dailyDemandCalculation.getDailyDemand() + VariableCaloriesDependingOnWeightChange.GAIN.getNumberOfCaloriesDependingOnWeightChange();
+                break;
+            case KEEP:
+                calculateCalories = dailyDemandCalculation.getDailyDemand() + VariableCaloriesDependingOnWeightChange.KEEP.getNumberOfCaloriesDependingOnWeightChange();
+                break;
+            default:
+                break;
+        }
+        return calculateCalories;
     }
 
-    private int CalculateDailyProtein() {
-        weight = new DailyDemandCalculation();
-        protein = 2 * weight.getWeight();
-        return protein;
+        private int calculateDailyProtein () {
+            weight = new DailyDemandCalculation();
+            protein = 2 * weight.getWeight();
+            return protein;
 
+        }
+
+        private int calculateDailyFat () {
+            fat = weight.getWeight();
+            return fat;
+
+        }
+
+        private double calculateDailyCarbs () {
+            double kcal = 0;
+            double calculateKcal = calculateMacro(caloriesDependingOnWeightChange);
+            kcal =  calculateKcal - ((fat * 9) + (protein * 4));
+            carbs = kcal / 4;
+            return Math.round(carbs * 100.0) / 100.0;
+        }
+
+        public int getProtein () {
+            return protein;
+        }
+
+        public void setProtein ( int protein){
+            this.protein = protein;
+        }
+
+        public int getFat () {
+            return fat;
+        }
+
+        public void setFat ( int fat){
+            this.fat = fat;
+        }
+
+        public double getCarbs () {
+            return carbs;
+        }
+
+        public void setCarbs ( double carbs){
+            this.carbs = carbs;
+        }
+
+    public VariableCaloriesDependingOnWeightChange getCaloriesDependingOnWeightChange() {
+        return caloriesDependingOnWeightChange;
     }
 
-    private int CalculateDailyFat() {
-        fat = weight.getWeight();
-        return fat;
-
+    public void setCaloriesDependingOnWeightChange(VariableCaloriesDependingOnWeightChange caloriesDependingOnWeightChange) {
+        this.caloriesDependingOnWeightChange = caloriesDependingOnWeightChange;
     }
 
-    private double CalculateDailyCarbs() {
-        double kcal = 0;
-        kcal = dailyDemandCalculation.getDailyDemand() - ((fat * 9) + (protein * 4));
-        carbs = kcal /4;
-        return Math.round(carbs * 100.0) / 100.0;
+    public DailyDemandCalculation getDailyDemandCalculation () {
+            return dailyDemandCalculation;
+        }
+
+        public void setDailyDemandCalculation (DailyDemandCalculation dailyDemandCalculation){
+            this.dailyDemandCalculation = dailyDemandCalculation;
+        }
+
+        public DailyDemandCalculation getWeight () {
+            return weight;
+        }
+
+        public void setWeight (DailyDemandCalculation weight){
+            this.weight = weight;
+        }
+
     }
-
-
-}
--- src/main/java/pl/sytomczak/nutritiontraining/gui/calculatedailydemand/CheckMacroView.java
+++ src/main/java/pl/sytomczak/nutritiontraining/gui/calculatedailydemand/CheckMacroView.java
@@ -1,12 +1,15 @@
 package pl.sytomczak.nutritiontraining.gui.calculatedailydemand;
 
+import pl.sytomczak.nutritiontraining.dailydemand.calculatemacro.CalculateMacro;
+import pl.sytomczak.nutritiontraining.dailydemand.calculatemacro.VariableCaloriesDependingOnWeightChange;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
-public class CheckMacroView extends JDialog{
+public class CheckMacroView extends JFrame {
     private JRadioButton gainWeightButton;
     private JRadioButton keepWeightButton;
     private JRadioButton loseWeightButton;
@@ -14,16 +17,28 @@ public class CheckMacroView extends JDialog{
     private JTextField proteinTextField;
     private JTextField fatTextField;
     private JTextField carbsTextField;
-    private JTextField textField4;
+    private JTextField kcalTextField;
     private JLabel proteinLabel;
     private JLabel fatLabel;
     private JLabel carbsLabel;
     private JLabel kcalLabel;
+    private JButton calculateButton;
+
+    private CalculateMacro calculateMacro;
 
     public CheckMacroView() {
+        calculateMacro = new CalculateMacro();
         setContentPane(checkMacroPanel);
         setTitle("Macro");
-        checkMacroPanel.setPreferredSize(new Dimension(600,300));
+        getRootPane().setDefaultButton(calculateButton);
+        checkMacroPanel.setPreferredSize(new Dimension(600, 300));
+
+        calculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onCalculate();
+            }
+        });
 
         checkMacroPanel.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -31,6 +46,30 @@ public class CheckMacroView extends JDialog{
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
+        gainWeightButton.addActionListener(e -> onSelectGainWeightButton());
+        keepWeightButton.addActionListener(e -> onSelectKeepWeightButton());
+        loseWeightButton.addActionListener(e -> onSelectLoseWeightButton());
+
+    }
+
+
+    private void onSelectLoseWeightButton() {
+        calculateMacro.setCaloriesDependingOnWeightChange(VariableCaloriesDependingOnWeightChange.LOSE);
+    }
+
+    private void onSelectKeepWeightButton() {
+        calculateMacro.setCaloriesDependingOnWeightChange(VariableCaloriesDependingOnWeightChange.KEEP);
+    }
+
+    private void onSelectGainWeightButton() {
+        calculateMacro.setCaloriesDependingOnWeightChange(VariableCaloriesDependingOnWeightChange.GAIN);
+    }
+
+    private void onCalculate() {
+        this.carbsTextField.setText(String.valueOf(calculateMacro.calculationMacro().getCarbs()));
+        this.fatTextField.setText(String.valueOf(calculateMacro.calculationMacro().getFat()));
+        this.proteinTextField.setText(String.valueOf(calculateMacro.calculationMacro().getProtein()));
+        this.kcalTextField.setText(String.valueOf(calculateMacro.calculationMacro().getCalories()));
     }
 
     private void onCancel() {
